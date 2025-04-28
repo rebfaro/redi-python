fd84c922a2a59e3a38e48df1e682308a48ae07d1
Rebeca Faro C. Victorino <rebfaro@gmail.com>
2025-04-28 15:53:06 +0200

    Lesson 3 - comments

--- lesson_03/5_dictionary_exercise.py
+++ lesson_03/5_dictionary_exercise.py
@@ -2,10 +2,11 @@
 #squares from 1 to their number and store them in a dictionary. Print out the dictionary at the
 #end to see the results.
 
+'''''
 number = int(input("Enter a number: "))
 squares = {}
-#count = 1
-'''''
+count = 1
+
 while count <= number:
     key = count
     value = count * count
@@ -16,6 +17,12 @@ print("----other option----")
 for k, v in squares.items():
     print(f"The square of {k} is {v}.") 
 '''
+
+# I did this exercise with while because It was easier for me, but I was having truble to understand the range() function
+# Then, I tried to make it, and that's what I got: 
+
+number = int(input("Enter a number: "))
+squares = {}
 
 for num in range (1 , number + 1):
     value = num * num
